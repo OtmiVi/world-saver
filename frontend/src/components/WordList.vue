b1dc68eb1a8e5c6841464aa6bcf445972330bc65
yura_kyziv <y.kyziv@elogic.com.ua>
2023-08-01 13:21:19 +0300

    Create word cards

--- frontend/src/components/WordList.vue
+++ frontend/src/components/WordList.vue
@@ -1,10 +1,28 @@
 <template>
-<h1>word list</h1>
+  <div class="row">
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+
+  </div>
+
+
 </template>
 
 <script>
+import WordCard from "@/components/WordList/WordCard.vue";
+
 export default  {
   name: 'WordList',
+  components: {
+    WordCard
+  },
 }
 </script>
 
