5780b4c16e454a8bb811dd7ca5856f148a9ed27d
yura_kyziv <y.kyziv@elogic.com.ua>
2023-08-01 15:24:28 +0300

    Add api request to get all words

--- frontend/src/components/WordList.vue
+++ frontend/src/components/WordList.vue
@@ -17,9 +17,32 @@
 
 <script>
 import WordCard from "@/components/WordList/WordCard.vue";
+import axios from "axios";
 
 export default  {
   name: 'WordList',
+  data() {
+    return {
+      words: Array,
+    }
+  },
+  created() {
+    this.getWords();
+  },
+  methods: {
+    async getWords() {
+      let url = "http://127.0.0.1:8000/api/words";
+      await axios.get(url).then(response => {
+        this.words = response.data;
+        console.log(this.words);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  mounted() {
+    console.log('mounted');
+  },
   components: {
     WordCard
   },
