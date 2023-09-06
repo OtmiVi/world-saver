60585fcf9cb8cf90b433ccd7e1673758aea45359
Yura Kuziv <kyzivyra.3@gmail.com>
2023-09-06 13:25:42 +0300

    Show words

--- frontend/src/components/WordList.vue
+++ frontend/src/components/WordList.vue
@@ -3,19 +3,8 @@
     <button type="button" class="btn btn-success m-1">Add new word</button>
   </router-link>
   <div class="row">
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-    <WordCard word="Test" translation="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-
+    <WordCard v-for="word in words" :key="word.id" :word=word.word :translation=word.translation />
   </div>
-
-
 </template>
 
 <script>
@@ -36,8 +25,7 @@ export default  {
     async getWords() {
       let url = "http://127.0.0.1:8000/api/words";
       await axios.get(url).then(response => {
-        this.words = response.data;
-        console.log(this.words);
+        this.words = response.data.words;
       }).catch(error => {
         console.log(error);
       });
